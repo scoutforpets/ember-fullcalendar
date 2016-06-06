9930ac5c4b86fe2f35d91c0c27c1bdcf4833778f
Dan Chadwick <dan899@gmail.com>
2016-06-05 22:40:19 -0400

    Allow opting-out of FullCalendar Scheduler.

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ module.exports = {
 
     app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.js');
     app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.css');
-    app.import(app.bowerDirectory + '/fullcalendar-scheduler/dist/scheduler.js');
-    app.import(app.bowerDirectory + '/fullcalendar-scheduler/dist/scheduler.css');
+
+    // Add scheduler to executable unless configured not to.
+    if (!app.options ||
+        !app.options.emberFullCalendar ||
+        app.options.emberFullCalendar.scheduler === undefined || app.options.emberFullCalendar.scheduler) {
+      app.import(app.bowerDirectory + '/fullcalendar-scheduler/dist/scheduler.js');
+      app.import(app.bowerDirectory + '/fullcalendar-scheduler/dist/scheduler.css');
+    }
   }
 };
