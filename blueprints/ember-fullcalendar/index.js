7f7856a5e5d33ea5c771bee72134f274d4f97746
Dan Chadwick <dan899@gmail.com>
2016-06-06 13:13:46 -0400

    Fix second regression in blueprint.

--- blueprints/ember-fullcalendar/index.js
+++ blueprints/ember-fullcalendar/index.js
@@ -2,7 +2,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('fullcalendar', '^2.7.3').then(function () {
+    return this.addBowerPackageToProject('fullcalendar', '^2.7.3').then(() => {
       return this.addBowerPackageToProject('fullcalendar-scheduler', '^1.3.2');
     });
   }
