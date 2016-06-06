926c050fcbc89c55a78cdb7ff975d19a8537850b
Dan Chadwick <dan899@gmail.com>
2016-06-06 13:06:21 -0400

    Fix blueprint regression.

--- blueprints/ember-fullcalendar/index.js
+++ blueprints/ember-fullcalendar/index.js
@@ -2,8 +2,8 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return addBowerPackageToProject('fullcalendar', '^2.7.3').then(function () {
-      return addBowerPackageToProject('fullcalendar-scheduler', '^1.3.2');
+    return this.addBowerPackageToProject('fullcalendar', '^2.7.3').then(function () {
+      return this.addBowerPackageToProject('fullcalendar-scheduler', '^1.3.2');
     });
   }
 };
