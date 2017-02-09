785b0f43f9568e8d342545438e96d4ed5ab4190a
James Dixon <jim.w.dixon@gmail.com>
2017-02-09 09:26:10 -0700

    use ember.assign and install polypill addon

--- addon/components/full-calendar.js
+++ addon/components/full-calendar.js
@@ -1,31 +1,8 @@
 import Ember from 'ember';
 import layout from '../templates/components/full-calendar';
 import { InvokeActionMixin } from 'ember-invoke-action';
-const { observer, computed, getOwner } = Ember;
 
-
-// We need IE Support so using a polyfill for Object.assign
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
-if (typeof Object.assign !== 'function') {
-  Object.assign = function(target) {
-    'use strict';
-    if (target == null) {
-      throw new TypeError('Cannot convert undefined or null to object');
-    }
-    target = Object(target);
-    for (var index = 1; index < arguments.length; index++) {
-      var source = arguments[index];
-      if (source !== null) {
-        for (var key in source) {
-          if (Object.prototype.hasOwnProperty.call(source, key)) {
-            target[key] = source[key];
-          }
-        }
-      }
-    }
-    return target;
-  };
-}
+const { assign, observer, computed, getOwner } = Ember;
 
 export default Ember.Component.extend(InvokeActionMixin, {
 
@@ -150,7 +127,7 @@ export default Ember.Component.extend(InvokeActionMixin, {
   didInsertElement() {
 
     const options =
-      Object.assign(
+      assign(
         this.get('options'),
         this.get('hooks')
       );
