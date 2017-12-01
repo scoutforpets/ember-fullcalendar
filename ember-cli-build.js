fcab3509224a956f1a934a2f78210bf92a258fd8
Federico Cortini <f.cortini@elsevier.com>
2017-12-01 15:53:49 +0000

    Adds support for google calendar.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/gcal.min.js');
+
   /*
     This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
